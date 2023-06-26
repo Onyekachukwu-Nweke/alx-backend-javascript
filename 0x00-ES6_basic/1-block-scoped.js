2e9a46af2031a91396f4339bb49c6def66df771a
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2023-06-26 14:41:43 -0700

    fix the block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,5 @@
 /* modified the initial function, to prevent overidding
-   of the varaibles in the if block
+ * of the varaibles in the if block
  */
 
 export default function taskBlock(trueOrFalse) {
@@ -7,8 +7,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+      const task = true; // eslint-disable-line
+      const task2 = false; //eslint-disable-line
     }
     return [task, task2];
-}
+  }
