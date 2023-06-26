a77e89d583d5ac9c1c414b75467ae53ef6b11b55
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2023-06-26 14:11:36 -0700

    hot fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,7 @@
+/* modified the initial function, to prevent overidding
+   of the varaibles in the if block
+ */
+
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
@@ -6,6 +10,5 @@ export default function taskBlock(trueOrFalse) {
       const task = true;
       const task2 = false;
     }
-  
     return [task, task2];
 }
