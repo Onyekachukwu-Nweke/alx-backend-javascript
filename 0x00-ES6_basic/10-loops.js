ca3db06b2097ef3f0acc37236e8495642f44b3ab
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2023-06-26 16:52:32 -0700

    feat: modifying loops using ES6 methods

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (let idx of array) {
+    const value = idx;
+    idx = appendString + value;
+    arr.push(idx);
+  }
+  return arr;
+}
