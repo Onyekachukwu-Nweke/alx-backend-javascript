62cf537ff1b43cd313dd767cb9724dd7c58ad1f1
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2023-06-30 16:44:08 -0700

    feat: Task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,23 @@
+/*
+ * Using the prototype below, return a promise. The parameter is a boolean.
+ *
+ * getFullResponseFromAPI(success)
+ * When the argument is:
+ *
+ * true
+ * resolve the promise by passing an object with 2 attributes:
+ * status: 200
+ * body: 'Success'
+ * false
+ * reject the promise with an error object with the message The fake API is not working currently
+ */
+export default getFullResponseFromAPI(success) {
+  const p = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return p;
+}
