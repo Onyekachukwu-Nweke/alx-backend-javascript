f6e7f53c5a931a5729442f8f452b1cab914fc196
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2023-07-05 20:40:24 -0700

    feat: task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+  const list = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+  const mappedList = new Map();
+  for (const item of list) {
+    mappedList.set(item[0], item[1]);
+  }
+  return mappedList;
+}
